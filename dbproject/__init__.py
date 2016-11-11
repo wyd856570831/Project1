0d137613533cb52e08861e8b80e93dc50c9fc3fd
Yuedong Wang <wyd856570831@qq.com>
2016-11-11 16:40:30 -0500

    port

--- dbproject/__init__.py
+++ dbproject/__init__.py
@@ -495,4 +495,31 @@ def delete_card():
 
 
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+
+    import click
+
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+    # """
+    # This function handles command line parameters.
+    # Run the server using
+
+    #     python server.py
+
+    # Show the help text using
+
+    #     python server.py --help
+
+    # """
+
+        HOST, PORT = host, port
+        print "running on %s:%d" % (HOST, PORT)
+        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+
+    run()
